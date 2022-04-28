8f1f041d8c6aea3770ce6b9414d98dc9daa1bae2
VasilicaClinca <vasilica.ciocarlan@gmail.com>
2022-04-28 12:43:55 +0100

    inheritance

---
+++ src/day38_Inheritance/carTask/car.java
@@ -0,0 +1,42 @@
+package day38_Inheritance.carTask;
+
+public class car {
+
+    public String brand, model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
+
+    public car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
+    public void start (){
+        System.out.println(brand + model + "is starting");
+    }
+    public void drive (){
+        System.out.println(brand + model + "is driving");
+    }
+
+    /*public void fly (){
+        System.out.println("");
+    }
+     */
+
+    public String toString() {
+        return "car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
+}
