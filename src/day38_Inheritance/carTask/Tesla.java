4e0a16b752f3a66eb65c37fe2a1d77c43d1a554e
VasilicaClinca <vasilica.ciocarlan@gmail.com>
2022-04-28 13:26:37 +0100

    parkingLot

--- src/day38_Inheritance/carTask/Tesla.java
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -1,11 +1,16 @@
 package day38_Inheritance.carTask;
 
 public class Tesla extends Car {
-    public Tesla(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public Tesla( String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
     }
 
     public void autoPilot(){
         System.out.println(brand + " " + model + "is in auto pilot mode");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Say \"Start\" to start " + brand+ " " + model);;
     }
 }
